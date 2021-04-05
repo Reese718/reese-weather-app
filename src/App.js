aa5e37d0abce00fc19ac82c5571d866d63100ff5
ReeseS <ReeseS718@users.noreply.github.com>
2021-04-04 20:16:48 -0400

    Added GitHub Url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by Reese Shider and is {""}
+        <a href="https://github.com/Reese718/weather-app-react" target="_blank">
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
