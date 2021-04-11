105aa933d3d2edc6f906fdb7bc2d38cede4442a6
ReeseS <ReeseS718@users.noreply.github.com>
2021-04-10 20:27:01 -0400

    added API intergration

--- src/Weather.js
+++ src/Weather.js
@@ -2,69 +2,63 @@ import React, {useState} from "react";
 import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
-
-const[ ready, setReady] = useState(false);
-const [ temperature, setTemperature] = useState(null);
-function handleResponse (response) {
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ready: false});
+  function handleResponse(response) {
     console.log(response.data);
-    setTemperature(response.data.main.temp);
-}
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+      feelsLike: response.data.main.feels_like,
+    });
+  }
 
-if (ready) { 
+  if (weatherData.ready) {
     return (
-    <div className="Weather">
-      <form>
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city"
+                className="search-form"
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-3">
+              <input type="submit" value="Search" className="btn btn-primary" />
+            </div>
+          </div>
+        </form>
+        <h1>{weatherData.city}</h1>
         <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Enter a city"
-              className="search-form"
-              autoFocus="on"
-            />
+          <div className="col-4">
+            <ul>
+              <li>Humidity: {weatherData.humidity}%</li>
+              <li>Wind: {Math.round(weatherData.wind)} km/h</li>
+              <li></li>
+            </ul>
           </div>
-          <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+          <div className="col-4">
+            <h2>Icon</h2>
           </div>
-        </div>
-      </form>
-      <h1>New York</h1>
-      <div className="row">
-        <div className="col-4">
-          <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-          </ul>
-        </div>
-        <div className="col-4">
-          <h2>Icon</h2>
-        </div>
-        <div className="col-4">
-          <h2>47°F</h2>
-          <p>Feels Like 34°</p>
+          <div className="col-4">
+            <h2>{Math.round(weatherData.temperature)}</h2>
+            <p>Feels Like {Math.round(weatherData.feelsLike)}°</p>
+          </div>
         </div>
       </div>
-    </div>
-);
-
-
-} else { 
+    );
+  } else {
     const apiKey = "0a8b11c35774ba923e07245d959e127d";
-  let city = "New York";
-  let apiUrl = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-  
-  return(
-      brooklyn
-  );
 
+    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=imperial`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading";
+  }
 }
-
-
-
-
-
-
-
-
